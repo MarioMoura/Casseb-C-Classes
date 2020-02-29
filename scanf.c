788b4f6664bfcd9e2878a071d8f2dc2cb98fed66
MarioMoura <mariohenriqueneto@gmail.com>
2020-02-29 20:51:38 -0300

    alualizacao scanf

--- scanf.c
+++ scanf.c
@@ -3,12 +3,25 @@
 int main(void){
 	int i;
 	char c;
-	float f;
+	float f = 0;
+	char s[20];
 
 	// o operador "&" significa o endereco a variavel 
 	// em sequencia
+	printf("Digite: int char float:");
 	scanf("%i %c %f", &i, &c, &f);
+	
+	// Complicado, mas em resumo:
+	// se sobrar input com o scanf o getchar
+	// vai comer ele 
+	int clear;
+	while((clear = getchar()) != '\n' && clear != EOF);
 
+	printf("Digite uma string:");
+	scanf("%s", s);
+	
+
+	puts(s);
 	printf("%i %c %f \n", i, c, f);
 
 	return 0;
